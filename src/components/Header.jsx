c0aa6e41a7e3f0a2d5fefdf36a95be81b3909098
crudeboy <jahswill.apata@gmail.com>
2022-09-30 14:29:21 +0100

    complete work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
 	return (
 		<>
 			<header>
+				<img style={{width: "200px", height: "40px"}}src={require("../images/Flixia-logo.png")} alt="" />
 				<i>Heavy Metal Movies</i>
 			</header>
 		</>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -6,15 +6,17 @@ function Heading() {
 		<>
 			<div className="header-container">
 				<Header />
-				<div className="heading-text-container">
-					<h1>Unlimited movies, TV shows, and more.</h1>
-					<p>Watch anywhere. Cancel anytime.</p>
-					<p>Ready to watch? Enter your email to create or restart your membership.</p>
-				</div>
-				<div className="sign-up">
-					<input className="input-element" type="text" name="fruit" placeholder="Enter your email" />
-					<input className="input-element" type="text" name="fruit" placeholder="Enter your password" />
-					<button type="submit">Submit</button>
+				<div className="header-wrapper">
+					<div className="heading-text-container">
+						<h1>Unlimited movies, TV shows, and more.</h1>
+						<p>Watch anywhere. Cancel anytime.</p>
+						<p>Ready to watch? Enter your email to create or restart your membership.</p>
+					</div>
+					<div className="sign-up">
+						<input className="input-element" type="text" name="fruit" placeholder="Enter your email" />
+						<input className="input-element" type="text" name="fruit" placeholder="Enter your password" />
+						<button type="submit">Submit</button>
+					</div>
 				</div>
 			</div>
 		</>
